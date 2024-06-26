b28fa43192069d5c277b172ff36f898bdf45599e
lvl0_module2 <zoey.lestyk@gmail.com>
2024-06-26 10:55:54 -0700

    jun 26

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -5,28 +5,34 @@ window = turtle.Screen()
 window.bgcolor('white')
 
 # This code makes a new Turtle. Pick a new name for the turtle
-my_turtle = turtle.Turtle()
+slowpoke = turtle.Turtle()
 
 # Make your turtle's shape 'turtle', .shape('turtle')
-
+slowpoke.shape('turtle')
 # Set your turtle's speed using .speed(2)
-
+slowpoke.speed(2)
 # Set your turtle's color using .color('green') and .pencolor('blue')
-
+slowpoke.color('green')
+slowpoke.pencolor('blue')
 # Move your turtle forward using .forward(100)
+slowpoke.forward(100)
 # TEST    Did your turtle move forward?
-
+slowpoke.left(90)
 # Move your turtle left or right using .left(90) or .right(90)
-
+for i in range(4):
+    slowpoke.left(90)
+    slowpoke.forward(100)
 # Now put the forward and left/right code into a for loop to repeat 4 times.
 # TEST    Did your turtle draw a square?
 
 # Move your turtle to a new place on the screen using .goto(x, y)
 # x=0 and y=0 is the center of the screen
-
+slowpoke.goto(0, 0)
+slowpoke.begin_fill()
 # Have your turtle draw a circle using .circle(radius, steps=30)
+slowpoke.circle(30, steps=30)
 # TEST    Did your turtle draw a circle?
-
+slowpoke.end_fill()
 # Add color to your shape by adding .begin_fill() before drawing the circle
 # and .end_fill() below
 
--- _02_code_flow/_a_turtle_spiral.py
+++ _02_code_flow/_a_turtle_spiral.py
@@ -14,23 +14,26 @@ if __name__ == '__main__':
     window.bgcolor('white')
     
     # Make a new turtle
-    
+    xineca = turtle.Turtle()
     # This code sets our shape to a turtle
-    
+    xineca.shape('turtle')
     # Set your turtle's speed (0=fastest, 1=slowest, 10=faster)
+    xineca.speed(10)
     
     # Set your turtle's color using .color('green')
-    
+    xineca.color('green')
     # Use a loop to repeat a the code below 50 times
-        
+    for i in range(250):
+        xineca.color(get_random_color())
+
         # Set the turtle color to a random color
 
         # Move the turtle (5*i) pixels. 'i' is the loop variable
-        
+        xineca.forward(5*1)
         # Turn the turtle (360/7) degrees to the right
-         
+        xineca.right(360/7)
         # Change the turtle width to 'i' (the loop variable)
-        
+        xineca.width(i)
         # Check the pattern against the picture in the recipe. If it matches, you are done!
     
 # ===================== DO NOT EDIT THE CODE BELOW ============================
